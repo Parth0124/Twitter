095c10b18d5ccb642554ae6c8d449c04d3c69225
Abhiraj412 <abhangparth@gmail.com>
2024-06-30 10:42:39 +0530

    added routes for login and home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom' 
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element= {<Home />}/>
+          <Route path='/login' element= {<Login />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
