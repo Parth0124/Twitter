2b9e1a0bb739ef247bb4fa4417c9ef9160584e60
Abhiraj412 <abhangparth@gmail.com>
2024-07-01 19:48:37 +0530

    added posts to feed

--- frontend/src/pages/Feed/Feed.jsx
+++ frontend/src/pages/Feed/Feed.jsx
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from "react";
 import "./Feed.css";
 import TweetBox from "./TweetBox/TweetBox";
+import Post from "./Post/Post";
 
 function Feed() {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        
+        fetch('http://localhost:4000/post')
+            .then(res => res.json())
+            .then(data => {
+                setPosts(data);
+            })
+    }, [posts])
+
     return (
         <div className="feed">
             <div className="feed__header">
                 <h2>Home</h2>
             </div>
             <TweetBox />
+            {
+                posts.map(p => <Post key={p._id} p={p} />)
+            }
 
         </div>
 
